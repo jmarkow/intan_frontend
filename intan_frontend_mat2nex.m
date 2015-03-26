e5a8092086c43d531f499713922ff2757b7b6991
Jeff Markowitz <markowitzmeister@gmail.com>
2015-03-26 18:12:10 -0400

    Bugfix and addded features to mat2nex

--- intan_frontend_mat2nex.m
+++ intan_frontend_mat2nex.m
@@ -1,4 +1,4 @@
-function intan_frontend_mat2nex(DATA,FS)
+function intan_frontend_mat2nex(EPHYS_DATA,EPHYS_FS,AUDIO_DATA,AUDIO_FS)
 % frontend takes a data matrix and writes an appropriate nex file
 %
 %
@@ -7,7 +7,7 @@ function intan_frontend_mat2nex(DATA,FS)
 %
 
 [storefile,storepath]=uiputfile('*.nex','New filename');
-[nsamples,ntrials,nchannels]=size(DATA);
+[nsamples,ntrials,nchannels]=size(EPHYS_DATA);
 
 % format the data for writing
 
@@ -15,21 +15,24 @@ disp('Reformatting data (this may take a minute)..');
 store_datavec=zeros(nsamples*ntrials,nchannels,'double'); 
 
 for i=1:nchannels
-	tmp=DATA(:,:,i);
+	tmp=EPHYS_DATA(:,:,i);
 	store_datavec(:,i)=tmp(:);
 end
 
 trial_stamps=1:ntrials;
-trial_stamps(2:end)=(trial_stamps(2:end)*FS)+1;
+trial_stamps(2:end)=(trial_stamps(2:end)*nsamples)+1;
 
 disp('Writing data...');
 nexfile=nexCreateFileData(40e3);
 
 for i=1:size(store_datavec,2)
-	nexfile=nexAddContinuous(nexfile,1/FS,FS,store_datavec(:,i),...
+	nexfile=nexAddContinuous(nexfile,1/EPHYS_FS,EPHYS_FS,store_datavec(:,i),...
 		[ 'CH ' num2str(i) ]);
 end
 
-nexfile=nexAddEvent(nexfile,trial_stamps/FS,'trialtimes');
-writeNexFile(nexfile,fullfile(storepath,storefile));
+store_audio=AUDIO_DATA(:);
 
+nexfile=nexAddContinuous(nexfile,1/AUDIO_FS,AUDIO_FS,store_audio,[ 'Microphone' ]);
+nexfile=nexAddEvent(nexfile,trial_stamps/EPHYS_FS,'trialtimes');
+
+writeNexFile(nexfile,fullfile(storepath,storefile));
